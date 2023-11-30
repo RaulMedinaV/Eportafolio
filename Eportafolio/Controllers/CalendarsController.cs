e35eeadbe0cc8ff2447660fbfadbccf7c24b6fb4
Raul <a198404@uabc.edu.mx>
2023-11-29 17:24:39 -0800

    Se incluyo CRUD para todos los modelos

---
+++ Eportafolio/Controllers/CalendarsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Eportafolio.Models;
+
+namespace Eportafolio.Controllers
+{
+    public class CalendarsController : Controller
+    {
+        private rmedinaEntities db = new rmedinaEntities();
+
+        // GET: Calendars
+        public ActionResult Index()
+        {
+            return View(db.Calendar.ToList());
+        }
+
+        // GET: Calendars/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Calendar calendar = db.Calendar.Find(id);
+            if (calendar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendar);
+        }
+
+        // GET: Calendars/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Calendars/Create
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Id,Title,Description,StartDate,EndDate,Location")] Calendar calendar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Calendar.Add(calendar);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(calendar);
+        }
+
+        // GET: Calendars/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Calendar calendar = db.Calendar.Find(id);
+            if (calendar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendar);
+        }
+
+        // POST: Calendars/Edit/5
+        // Para protegerse de ataques de publicación excesiva, habilite las propiedades específicas a las que quiere enlazarse. Para obtener 
+        // más detalles, vea https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Title,Description,StartDate,EndDate,Location")] Calendar calendar)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(calendar).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(calendar);
+        }
+
+        // GET: Calendars/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Calendar calendar = db.Calendar.Find(id);
+            if (calendar == null)
+            {
+                return HttpNotFound();
+            }
+            return View(calendar);
+        }
+
+        // POST: Calendars/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Calendar calendar = db.Calendar.Find(id);
+            db.Calendar.Remove(calendar);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
